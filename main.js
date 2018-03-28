0d726e76cf4f4586c4123ab7fd1b6d4cb1507c17
Harsh Shah <harsh.shah@sapientrazorfish.com>
2018-03-28 12:39:12 +0530

    Promise added

--- main.js
+++ main.js
@@ -61,22 +61,22 @@ let questions = [
         name: 'Unit_16',
       },
     ],
-    validate: function(answer) {
+    validate: function (answer) {
       if (answer.length < 1) {
         return 'You need to select atleast 1 unit';
       }
       return true;
     },
-  },{
-      type: 'input',
-      name: 'filename',
-      message: 'Name of the output file (default : Unit-config.js)',
-      validate: function(answer){
-        if(answer.length < 0){
-            answer.filename = 'Unit-config'
-        }
-        return true;
+  }, {
+    type: 'input',
+    name: 'filename',
+    message: 'Name of the output file (default : Unit-config.js)',
+    validate: function (answer) {
+      if (answer.length < 0) {
+        answer.filename = 'Unit-config'
       }
+      return true;
+    }
   }
 ];
 
@@ -87,45 +87,49 @@ program
   .description('Working...')
   .action(() => {
     inquirer.prompt(questions).then(answer => {
-      
-     if(answer.filename == ""){
-         answer.filename = 'Unit-config';
-     }
-      fs.truncate(`dist/${answer.filename}.js`, 0, err => {});
+
+      if (answer.filename == "") {
+        answer.filename = 'Unit-config';
+      }
+      fs.truncate(`dist/${answer.filename}.js`, 0, err => { });
       var unitsArr = [];
       var unitsObj = {};
-      answer.units.forEach(function(element) {
+      answer.units.forEach(function (element) {
         unitsObj += element;
         let splitElem = element.split('_');
         unitsArr.push(splitElem[splitElem.length - 1]);
       }, this);
 
       Promise.resolve(
-        unitsArr.forEach(function(element) {
+        unitsArr.forEach(function (element) {
           let importStr = `import unit${element} from Unit_${element};\n`;
           fs.appendFile(`dist/${answer.filename}.js`, importStr, (err, data) => {
             if (!err) {
               console.log(chalk.blue(`Written Unit ${element}`));
             }
           });
-        }, this),
-      ).then(function() {
-        let exportStatement = `\n\nexport default {\n`;
-        let exportStatementEnd = `};`;
-        fs.appendFile(`dist/${answer.filename}.js`, exportStatement, () => {});
-        answer.units.forEach(function(element) {
-          let unitExportStatement = `\t${element},\n`;
-          fs.appendFile(`dist/${answer.filename}.js`, unitExportStatement, () => {});
-        }, this);
-        fs.appendFile(`dist/${answer.filename}.js`, exportStatementEnd, () => {});
-        console.log(chalk.bold.green('Generated Config file in dist folder'))
-        figlet('By Bunnyweb', function(err, data){
-            if(err){
-                return 
+        }, this)).then(function () {
+
+          function exportStatements() {
+            let exportStatement = `\n\nexport default {\n`;
+            let exportStatementEnd = `};`;
+            fs.appendFile(`dist/${answer.filename}.js`, exportStatement, () => { });
+            Promise.resolve(answer.units.forEach(function (element) {
+              let unitExportStatement = `\t${element},\n`;
+              fs.appendFile(`dist/${answer.filename}.js`, unitExportStatement, () => { });
+            }, this)).then(function () {
+              fs.appendFile(`dist/${answer.filename}.js`, exportStatementEnd, () => { });
+            });
+            console.log(chalk.bold.green('Generated Config file in dist folder'))
+          }
+          setTimeout(exportStatements, 600);
+          figlet('By Bunnyweb', function (err, data) {
+            if (err) {
+              return
             }
             console.log(data)
+          });
         });
-      });
     });
   });
 
